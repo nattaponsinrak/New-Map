8987c3d371b2cc3cdf1c23b86465581464d02e54
masterUNG <nattapon_pook@hotmail.com>
2016-06-23 11:55:12 +0700

    up data Create Point

--- New NEU/ViewController.swift
+++ New NEU/ViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var myMapView: MKMapView!
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+
+        var myLat:CLLocationDegrees = 16.422701
+        var myLng:CLLocationDegrees = 102.815245
+        
+        var latDelta:CLLocationDegrees = 0.01
+        var lngDelta:CLLocationDegrees = 0.01
+        
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lngDelta)
+        var neuLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLng)
+        
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMake(neuLocation, theSpan)
+        
+        
+        self.myMapView.setRegion(theRegion, animated : true)
+        
+            var neuAnotation = MKPointAnnotation
+            neuAnotation.Coordinate = neuLocation
+            neuAnotation.title = "มหาลัยภาค จังหวัดขอนแก่น"
+            neuAnotation.subtitle = "จังหวัดขอนแก่น"
+        
+        self.myMapView.addAnnotation(neuAnotation)
+        
+    }  /// view didload ค้นหาพิกัด Map by GoogleView
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
